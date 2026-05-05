519cbd17055968c81026857b3283620c9a90233a
iaysa45 <kaysa.kamila57@gmail.com>
2026-05-04 21:10:40 -0700

    Update database.php

--- config/database.php
+++ config/database.php
@@ -1,1 +1,18 @@
+<?php
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_NAME', 'uts_perpustakaan_[60324004]');
 
+$conn = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+
+if ($conn->connect_error) {
+    die("Koneksi gagal: " . $conn->connect_error);
+}
+
+$conn->set_charset("utf8mb4");
+
+function escape($conn, $data) {
+    return htmlspecialchars($conn->real_escape_string($data),ENT_QUETES, 'UTF-8');
+}
+?>
